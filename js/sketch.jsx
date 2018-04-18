dff2ee2862a95a235adf4436a03690dbafbcfbea
wrola <rola.wojciech@gmail.com>
2018-04-18 10:47:14 +0200

    działa wybór drużyny z API

--- js/sketch.jsx
+++ js/sketch.jsx
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom';
 class Header extends React.Component {
     constructor(props) {
         super(props);
-
     }
-
     render() {
-
         return (
             <div>
                 <h1>The Bet Game</h1>
@@ -18,28 +15,6 @@ class Header extends React.Component {
     }
 }
 
-class ChooseTeam extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            team: 'Lechia'
-        }
-    }
-
-    render(){
-    console.log(this.props.teams)
-    return(
-            <div>
-                <label>Team</label>
-                <select>
-                <option>Choose a team</option>
-                    {this.props.teams.map((elem)=><option>{elem.name}</option>)}
-            </select>
-            </div>
-            )
-}
-}
-
 class Bet extends React.Component {
     constructor(props) {
         super(props);
@@ -47,51 +22,68 @@ class Bet extends React.Component {
             stake: 0
         }
     }
+    performFetch1 = () => {
+    }
 
     render() {
-
-        return (
-            <div>
-                <p>
-                    {this.state.stake}
-                </p>
+        return (  <div>
+                <h2>The bet: {this.state.stake} </h2>
                 <input type='number'/>
             </div>
         )
     }
 }
 
-const teams = [
-    {name: 'Lechia Gdańsk'},
-    {name: 'Wisła Kraków'},
-    {name: 'Legia Warszawa'},
-    {name: 'Arka Gdynia'},
-    {name: 'Lech Poznań'},
-    {name: 'Jagielonia Białystok'},
-    {name: 'Górnik Zabrze'},
-    {name: 'Pogoń Szczecin'},
-    {name: 'Zagłębie Lubin'},
-    {name: "Wisła Płock"},
-    {name: 'Korona Kielce'},
-    {name: 'Cracovia'},
-    {name: 'Śląsk Wrocław'},
-    {name: 'Piast Gliwice'},
-    {name: 'Sandecja Nowy Sącz'},
-    {name: 'Brul-bet Nieciecza'}
-]
+class ChooseTeam extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            team: '',
+            options: [{name: "Janusze Kodu"}]
+        }
+    }
+
+    componentDidMount = () =>{
+        fetch('http://api.football-data.org/v1/competitions/398/teams', {
+            headers: {'X-Auth-Token': '405e8d17c66e46e284d542c0fb7aacd5'},
+            dataType: 'json'
+        }).then(r => r.json()).then(data => {
+            //console.log(data.teams);
+            this.setState({
+                options: data.teams
+            })
+        }).catch(e => {
+            console.log('Błąd!!!!', e)
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <label>Team</label>
+                <select>
+                    <option>Choose a team</option>
+                    {this.state.options.map((elem, i) => <option key={i}>{elem.name}</option>)}
+                </select>
+                <h2>Oponent
+                    {/*{this.props.api.map((elem,i)=> elem. )}*/}
+                </h2>
+            </div>
+        )
+    }
+}
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-
     }
-
     render() {
-
         return (
             <div>
-                <header/>
-                <ChooseTeam teams={teams}/>
+                <Header/>
+                <ChooseTeam/>
                 <Bet/>
             </div>
         )
@@ -104,10 +96,3 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('app')
     );
 })
-
-
-// lista zespołów z kolejki wyświetla się na poczatku wpisujesz ile chcesz obstawić,
-// następnie na kilk na nazwę zespołu obstawiasz i msz gotowy zakład.
-//wyśiwetlić musisz aktualna kolejkę i obok nazwy kurs na wygraną zespołu
-//algorytm na obliczanie szans na wygraną przegraną lub remis.
-//jak zapamietac wyniki ? jak ma je trzmyać w pamięci ?
