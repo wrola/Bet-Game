1e53d76872f4414871a2eca69055a3da2424fc23
wrola <rola.wojciech@gmail.com>
2018-05-02 14:42:07 +0200

    add bet

--- js/sketch.jsx
+++ js/sketch.jsx
@@ -28,8 +28,8 @@ class Main extends React.Component {
             tableLeague: [],
             stake: 0,
             bet: 0,
-            dateOfValid: '',
-            OponentOfTeam: '',
+            newItem: '',
+            list: []
         }
     }
 
@@ -74,7 +74,7 @@ class Main extends React.Component {
             this.setState({
                 oponents: data.fixtures,
             })
-            console.log(this.state.oponents)
+            // console.log(this.state.oponents)
         }).catch(e => {
             console.log('Błąd!!!!', e)
         });
@@ -85,6 +85,31 @@ class Main extends React.Component {
         this.setState({
             bet: result
         })
+    }
+    updateInput = (key, value) => {
+        this.setState({
+            [key]: value
+        });
+        localStorage.setItem(key, value)
+    }
+    addItem = () => {
+        // create a new item
+        const newItem = {
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+        };
+
+        // copy current list of items
+        const list = [...this.state.list];
+
+        // add the new item to the list
+        list.push(newItem);
+
+        // update state with new list, reset the new item input
+        this.setState({
+            list,
+            newItem: ""
+        });
     }
     handleSubmit = (e) => {
         e.preventDefault();
@@ -123,9 +148,20 @@ class Main extends React.Component {
                         }
                     })
                 };
-                    <input type='text' onChange={this.handleBet}/>
-                    <button type='button'>BET</button>
+                    <input type='text' value={this.state.newItem} onChange={e=> this.updateInput('newItem', e.target.value)}/>
+                    <button type='button' onClick={()=>this.addItem()}>BET</button>
                 </form>
+                <ul>
+                    {this.state.list.map(item => {
+                        return (
+                            <li key={item.id}>
+                                {item.value}
+                                {item.team}
+
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         )
     }
