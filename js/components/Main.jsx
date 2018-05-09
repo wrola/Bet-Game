bfc26e204e7839bcc79039ef5ca189b50f9a0208
wrola <rola.wojciech@gmail.com>
2018-05-09 12:15:52 +0200

    clean up the code

--- js/components/Main.jsx
+++ js/components/Main.jsx
@@ -13,15 +13,16 @@ class Main extends React.Component {
         }
     }
 
-    componentDidMount = () =>{
+    componentDidMount = () => {
         fetch('http://api.football-data.org/v1/competitions/398/teams', {
             headers: {'X-Auth-Token': '405e8d17c66e46e284d542c0fb7aacd5'},
             dataType: 'json'
         }).then(r => r.json()).then(data => {
             const chances = ((Math.random() * 10) + 1).toFixed(2);
-            if(toString(chances).length > 4){
+            if (toString(chances).length > 4) {
                 Math.round(chances)
-            };
+            }
+            ;
             this.setState({
                 options: data.teams,
             })
@@ -31,19 +32,19 @@ class Main extends React.Component {
         });
         this.updateStateWithLocalStorage()
     }
-    handleOption = (event) =>{
-        this.state.options.forEach((elem,i)=>{
-                if(elem.name === event.target.value ){
+    handleOption = (event) => {
+        this.state.options.forEach((elem, i) => {
+                if (elem.name === event.target.value) {
                     this.fetchOpponentsFromAPI(elem._links.fixtures.href);
                 }
             },
             this.setState({
-                team : event.target.value
+                team: event.target.value
             })
         )
     }
-    fetchOpponentsFromAPI = (url) =>{
-        fetch(url,{
+    fetchOpponentsFromAPI = (url) => {
+        fetch(url, {
             headers: {'X-Auth-Token': '405e8d17c66e46e284d542c0fb7aacd5'},
             dataType: 'json'
         }).then(r => r.json()).then(data => {
@@ -69,7 +70,7 @@ class Main extends React.Component {
 
     updateInput = (key, value) => {
         const chances = ((Math.random() * 6) + 1).toFixed(2);
-        const result =(Math.abs(value * chances)).toFixed(2);
+        const result = (Math.abs(value * chances)).toFixed(2);
         this.setState({
             [key]: result
         });
@@ -101,6 +102,7 @@ class Main extends React.Component {
         localStorage.setItem('list', JSON.stringify(list));
         localStorage.setItem('newItem', '');
     }
+
     deleteItem(id) {
         // copy current list of items
         const list = [...this.state.list];
@@ -116,18 +118,19 @@ class Main extends React.Component {
             updatedList
         ));
     }
-    updateStateWithLocalStorage = () =>{
-        for(let key in this.state){
-            if(localStorage.hasOwnProperty(key)){
+
+    updateStateWithLocalStorage = () => {
+        for (let key in this.state) {
+            if (localStorage.hasOwnProperty(key)) {
                 let value = localStorage.getItem(key);
                 try {
                     value = JSON.parse(value);
                     this.setState({
-                        [key] : value
+                        [key]: value
                     })
                 } catch (e) {
                     this.setState({
-                        [key] : value
+                        [key]: value
                     })
                 }
             }
@@ -137,21 +140,23 @@ class Main extends React.Component {
     render() {
         return <div className="main">
             <form type="submit" className='main-form'>
-                <p>
-                    Choose a team
-                </p>
-               <Slider options={this.state.options} selectedTeam={this.handleOption}/>
+                    <p>
+                        Choose a team
+                    </p>
+                <Slider options={this.state.options} selectedTeam={this.handleOption}/>
+
                 <h2>Oponents</h2>
+
                 <input className='main-form-input' type='text'
                        onChange={e => this.updateInput('newItem', e.target.value)}/>
                 <button className='main-form-btn btn' type='button' onClick={() => this.addItem()}>BET</button>
             </form>
-            <ul className="main-banner">
-                {this.state.list.map(item => {
-                    return (
-                        <li className='main-banner-el' key={item.id}>
 
-                            I bet {item.value} for win {item.team} in game with {item.oponent} on {item.date}
+            <ul className="main-banner">
+                    {this.state.list.map(item => {
+                        return (
+                            <li className='main-banner-el' key={item.id}>
+                                I bet {item.value} for win {item.team} in game with {item.oponent} on {item.date}
                             <button className='main-banner-btn btn' onClick={() => this.deleteItem(item.id)}>
                                 Remove
                             </button>
@@ -163,4 +168,5 @@ class Main extends React.Component {
         </div>
     }
 }
+
 export default Main;
--- js/components/Slider.jsx
+++ js/components/Slider.jsx
@@ -9,27 +9,26 @@ class Slider extends React.Component{
             oponents: '',
         }
     }
-    const
+    nextSlide = () => {
+
+    };
+    prevSlide = () => {
+
+    };
     render() {
 
         return (
             <div className="main-form-slider" style={{display: 'flex', justifyContent: 'space-around'}}>
-                <button type="button" className="main-form-prev" style={{background: 'transparent', border:'0', color: '#00134d',background: 'transparent',transition: '0.5s color',cursor:'pointer'}}>
+                <button type="button" onClick={this.prevSlide} className="main-form-prev" style={{background: 'transparent', border:'0', color: '#00134d',background: 'transparent',transition: '0.5s color',cursor:'pointer'}}>
                     &lt;
                 </button>
                 <label>Team</label>
                 <select onChange={this.props.selectedTeam}>
                     <option>Choose a team</option>
-                    {this.props.options.map((elem, i) => <option key={i}>{elem.name}</option>)}
+                    {this.props.options.map((elem, i) => <option key={i} className='main-form-option' >{elem.name}</option>)}
                 </select>
-                {/*{this.props.options.map((elem, i) => {*/}
-                    {/*return (<div className='main-form-slide' style={{opacity:'1'}}>*/}
-                        {/*<img className='main-form-slide-img' style={{height:'50px', width: '50px'}} key={i} src={elem.crestUrl}/>*/}
-                        {/*<span className='main-form-slide-title'>{elem.name}</span>*/}
-                    {/*</div>)*/}
-                {/*})}*/}
 
-                <button type="button" className="main-form-next" style={{background: 'transparent', border:'0', color: '#00134d',background: 'transparent',transition: '0.5s color',cursor:'pointer'}}>
+                <button type="button" className="main-form-next" onClick={this.nextSlide} style={{background: 'transparent', border:'0', color: '#00134d',background: 'transparent',transition: '0.5s color',cursor:'pointer'}}>
                     &gt;
                 </button>
             </div>
